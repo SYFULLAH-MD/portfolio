5cc6cf9606eaa5106c118c28d26440243ad44772
SYFULLAH-MD <syfullahsyfu789@gmail.com>
2025-03-15 18:15:10 -0500

    8

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 // Toggle Sidebar Menu
 function toggleMenu() {
     let sidebar = document.getElementById("sidebar");
+    let overlay = document.getElementById("sidebar-overlay");
+
     if (sidebar.style.left === "0px") {
-        sidebar.style.left = "-250px";
+        sidebar.style.left = "-250px"; // Close sidebar
+        overlay.style.display = "none";
     } else {
-        sidebar.style.left = "0px";
+        sidebar.style.left = "0px"; // Open sidebar
+        overlay.style.display = "block";
     }
 }
