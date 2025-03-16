e18936ffec614822361207dfec42785b407f8ffa
SYFULLAH-MD <syfullahsyfu789@gmail.com>
2025-03-15 19:36:21 -0500

    guess fix

--- js/script.js
+++ js/script.js
@@ -1,9 +1,6 @@
-// Toggle Sidebar Menu
+// Toggle the dropdown menu inside the hamburger
 function toggleMenu() {
-    let sidebar = document.getElementById("sidebar");
-    if (sidebar.style.left === "0px") {
-        sidebar.style.left = "-250px";
-    } else {
-        sidebar.style.left = "0px";
-    }
-}
+    let hamburger = document.querySelector(".hamburger");
+    hamburger.classList.toggle("active");
+  }
+  
